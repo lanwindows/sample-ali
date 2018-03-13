96da37ba764f94c85e96b08032f6710f5539625f
Lee <lw9080@163.com>
2018-03-13 17:45:23 +0800

    finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App') - Laravel开发</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
-    {{--表示该占位区域将用于显示 content 区块的内容，而 content 区块的内容将由继承自 default 视图的子视图定义。--}}
+    @include('layouts._header')
+    {{--引用头部视图resources/views/layouts/_header.blade.php--}}
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content'){{--定义content区块--}}
+        @include('layouts._footer')
+          {{--引用尾部视图resources/views/layouts/_footer.blade.php--}}
+      </div>
+    </div>
   </body>
 </html>
